8b644856b9eca6ad2e24ef7539e0359be09c6d92
Atikur Rahman <atikurrahman160313@gmail.com>
2026-02-22 07:31:33 +0600

    Add solution for Remove Element problem

---
+++ LeetCode-Problem-27-Remove-Element.js
@@ -0,0 +1,16 @@
+// Remove Elements
+function removeElements(nums, k) {
+  let left = 0;
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] !== k) {
+      nums[left] = nums[right];
+      left++;
+    }
+  }
+  return left;
+}
+
+let Arry = [3, 2, 2, 3];
+let Length = removeElements(Arry, 3);
+console.log(Length);
+console.log(Arry.slice(0, Length));
