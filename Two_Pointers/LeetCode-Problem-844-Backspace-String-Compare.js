fedf521f657c7dd015fe410a915d9cc0b18d7d1f
Atikur Rahman <atikurrahman160313@gmail.com>
2026-03-28 06:35:54 +0600

    Refactor backspaceCompare function to use two-pointer technique for string comparison

--- Two_Pointers/LeetCode-Problem-844-Backspace-String-Compare.js
+++ Two_Pointers/LeetCode-Problem-844-Backspace-String-Compare.js
@@ -1,26 +1,66 @@
-function Backspace (str) {
-    let stack = [];
-    for (let char of str) {
-        if (char !== '#') {
-            stack.push(char)
+function backspaceCompare(s, t) {
+    // Two pointers - end theke start korchi (backspace effect easier handle er jonno)
+    let i = s.length - 1;  // s string er last index
+    let j = t.length - 1;  // t string er last index
+    
+    // Jotokhon na duita pointer e shesh hoye jay (both -1)
+    while (i >= 0 || j >= 0) {
+        
+        // ========== S string er next valid character find ==========
+        let skipS = 0;  // Koyta backspace skip korte hobe tar counter
+        
+        while (i >= 0) {
+            if (s[i] === '#') {
+                // Backspace peye gele, skip count barabo
+                skipS++;
+                i--;
+            } else if (skipS > 0) {
+                // Backspace er por normal character peye gele, eta skip korte hobe
+                skipS--;
+                i--;
+            } else {
+                // Backspace nei, and skip kora lagbe na - eta valid character
+                break;
+            }
         }
-        else {
-            stack.pop()
+        
+        // ========== T string er next valid character find ==========
+        let skipT = 0;  // T er jonno skip counter
+        
+        while (j >= 0) {
+            if (t[j] === '#') {
+                skipT++;
+                j--;
+            } else if (skipT > 0) {
+                skipT--;
+                j--;
+            } else {
+                break;
+            }
         }
+        
+        // ========== Compare valid characters ==========
+        
+        // Case 1: Duita string e valid character ache
+        if (i >= 0 && j >= 0) {
+            if (s[i] !== t[j]) {
+                return false;  // Character mismatch
+            }
+        } 
+        // Case 2: Ekta string e valid character ache, arekta te nei
+        else if (i >= 0 || j >= 0) {
+            return false;  // Length mismatch
+        }
+        // Case 3: Duitai sesh (both -1) - continue kore next iteration e check korbe
+        
+        // Pointers move koro next comparison er jonno
+        i--;
+        j--;
     }
-    return stack.join('');
-
+    
+    // Now strings are equal after backspace
+    return true;
 }
 
-function checkBackspace (s, t) {
-    if (s !== t) {
-        return false;
-    }
-    return true
-}
-
-let str1 = "ab##";
-console.log("Str1(s) : ", Backspace(str1) )
-let str2 =  "c#d#";
-console.log("Str2(t) : ", Backspace(str2) )
-console.log("Does it Matched? : ", checkBackspace(Backspace(str1), Backspace(str2)))
+// Test koro
+console.log(backspaceCompare("ab##", "c#d#")); 
